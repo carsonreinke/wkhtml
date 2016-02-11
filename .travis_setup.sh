a859f232c909d25b58ffc43fcc712227999fea22
carsonreinke <carson@reinke.co>
2016-02-10 22:05:57 -0500

    Another attempt

--- .travis_setup.sh
+++ .travis_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz > /dev/null
 tar xf wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
 echo "wkhtmltopdf: `./wkhtmltox/bin/wkhtmltopdf -V`"
-echo "wkhtmltopdf: `./wkhtmltox/bin/wkhtmltoimage -V`"
+echo "wkhtmltoimage: `./wkhtmltox/bin/wkhtmltoimage -V`"
 
+bundle install
 bundle exec rake compile -- --with-wkhtmltox-dir=./wkhtmltox/
